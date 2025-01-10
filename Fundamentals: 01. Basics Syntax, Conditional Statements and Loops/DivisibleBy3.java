65db1ca6013dcf4fe1db0e29faa72d9352965d33
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-10 23:33:21 +0100

    Create DivisibleBy3.java

---
+++ Fundamentals: 01. Basics Syntax, Conditional Statements and Loops/DivisibleBy3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DivisibleBy3 {
+    public static void main(String[] args) {
+    
+
+        for (int number = 1; number <= 100; number++) {
+            if (number % 3 == 0) {
+                System.out.println(number);
+            }
+        }
+
+    }
+}
