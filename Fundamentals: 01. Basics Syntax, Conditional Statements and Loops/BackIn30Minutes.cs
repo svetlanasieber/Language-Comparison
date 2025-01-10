7fd1e5db45b4d991e03f43ee957e57317444bc33
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-10 23:23:01 +0100

    Create BackIn30Minutes.cs

---
+++ Fundamentals: 01. Basics Syntax, Conditional Statements and Loops/BackIn30Minutes.cs
@@ -0,0 +1,16 @@
+int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+minutes += 30;
+
+if (minutes > 59)
+{
+    minutes -= 60;
+    hours++;
+}
+if (hours > 23) {
+
+    hours = 0;
+}
+
+Console.WriteLine($"{hours}:{minutes:D2}");
