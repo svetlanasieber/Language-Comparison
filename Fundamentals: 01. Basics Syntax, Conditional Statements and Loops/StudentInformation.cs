8b5eae64e0a845ca7203482c86a5c3c0a4f4641e
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-10 23:21:14 +0100

    Create StudentInformation.cs

---
+++ Fundamentals: 01. Basics Syntax, Conditional Statements and Loops/StudentInformation.cs
@@ -0,0 +1,5 @@
+string studentName = Console.ReadLine();
+int studentAge = int.Parse(Console.ReadLine());
+double studentGrade = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Name: {studentName}, Age: {studentAge}, Grade: {studentGrade:f2}");
