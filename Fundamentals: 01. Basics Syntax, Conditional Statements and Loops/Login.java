4466b756b44cf25800ca92982399456b782e88c1
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-12 16:12:33 +0100

    Update Login.java

--- Fundamentals: 01. Basics Syntax, Conditional Statements and Loops/Login.java
+++ Fundamentals: 01. Basics Syntax, Conditional Statements and Loops/Login.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class Login_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String username = scanner.nextLine();
+        String password = ""; 
+
+        for (int position = username.length() - 1; position >= 0; position--) {
+           
+            char symbol = username.charAt(position); 
+            password += symbol; //password = password + symbol;
+        }
+
+        String enteredPassword = scanner.nextLine();
+        int countWrongAttempts = 0; 
+        while (!enteredPassword.equals(password)) {
+            countWrongAttempts++;
+            if (countWrongAttempts == 4) {
+                System.out.printf("User %s blocked!", username);
+                return; 
+            }
+            System.out.println("Incorrect password. Try again.");
+            enteredPassword = scanner.nextLine();
+        }
+        System.out.printf("User %s logged in.", username);
+    }
+}
