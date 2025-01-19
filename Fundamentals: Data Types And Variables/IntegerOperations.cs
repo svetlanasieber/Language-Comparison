bbf274a88f7e9f48d8521fc343a39c0ca3112416
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-19 19:48:27 +0100

    Create IntegerOperations.cs

---
+++ Fundamentals: Data Types And Variables/IntegerOperations.cs
@@ -0,0 +1,15 @@
+using System.Numerics;
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+int num4 = int.Parse(Console.ReadLine());
+
+
+
+int firstResult = num1 + num2;
+int secondResult = firstResult / num3;
+int thirdResult = secondResult * num4;
+
+
+Console.WriteLine(thirdResult);
