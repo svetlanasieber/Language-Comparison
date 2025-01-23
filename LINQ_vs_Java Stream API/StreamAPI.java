04049cbbc7a41b27e0f72c8ba62e383f866fd956
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-23 03:26:55 +0100

    Create StreamAPI.java

---
+++ LINQ_vs_Java Stream API/StreamAPI.java
@@ -0,0 +1,4 @@
+List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+List<Integer> evenNumbers = numbers.stream()
+                                   .filter(n -> n % 2 == 0)
+                                   .collect(Collectors.toList());
